59c99952c7e8f780507196bfad808cf85fbd3765
Daniel Agapov <danielagapov1@gmail.com>
2025-03-23 04:30:59 -0700

    Revert "styling for centering"
    This reverts commit 4bf0635a7375d4171e22a82d0ad48469e24eae87.

--- compass-wrapped-frontend/src/pages/HomePage.tsx
+++ compass-wrapped-frontend/src/pages/HomePage.tsx
@@ -7,18 +7,20 @@ const HomePage: React.FC = () => {
   const uploadedContent = '';
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-between relative">
+    <div className="min-h-screen flex flex-col items-center justify-between relative !w-full">
       <Background />
       
-      <main className="flex-1 w-full flex flex-col items-center justify-center py-8 px-4 text-center">
-        <div className="w-full max-w-3xl mx-auto flex flex-col items-center gap-12">
-          <Header />
+      <main className="flex-1 !w-full flex flex-col items-center justify-center py-8 px-4 text-center">
+        <div className="!w-full max-w-3xl mx-auto flex flex-col items-center gap-12">
+          <div className="!w-full flex justify-center">
+            <Header />
+          </div>
           
-          <div className="w-full flex justify-center">
+          <div className="!w-full flex justify-center">
             <FileDropzone />
           </div>
           
-          <div className="w-full flex justify-center">
+          <div className="!w-full flex justify-center">
             <CSVInstructions 
               uploading={uploading} 
               setUploading={setUploading} 
