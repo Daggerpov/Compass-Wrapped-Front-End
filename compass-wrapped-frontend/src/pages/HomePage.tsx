4bf0635a7375d4171e22a82d0ad48469e24eae87
Daniel Agapov <danielagapov1@gmail.com>
2025-03-23 04:30:52 -0700

    styling for centering

--- compass-wrapped-frontend/src/pages/HomePage.tsx
+++ compass-wrapped-frontend/src/pages/HomePage.tsx
@@ -7,20 +7,18 @@ const HomePage: React.FC = () => {
   const uploadedContent = '';
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-between relative !w-full">
+    <div className="min-h-screen flex flex-col items-center justify-between relative">
       <Background />
       
-      <main className="flex-1 !w-full flex flex-col items-center justify-center py-8 px-4 text-center">
-        <div className="!w-full max-w-3xl mx-auto flex flex-col items-center gap-12">
-          <div className="!w-full flex justify-center">
-            <Header />
-          </div>
+      <main className="flex-1 w-full flex flex-col items-center justify-center py-8 px-4 text-center">
+        <div className="w-full max-w-3xl mx-auto flex flex-col items-center gap-12">
+          <Header />
           
-          <div className="!w-full flex justify-center">
+          <div className="w-full flex justify-center">
             <FileDropzone />
           </div>
           
-          <div className="!w-full flex justify-center">
+          <div className="w-full flex justify-center">
             <CSVInstructions 
               uploading={uploading} 
               setUploading={setUploading} 
